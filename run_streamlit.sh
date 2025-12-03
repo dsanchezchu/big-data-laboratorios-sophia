b6dc266f8178770010520fe3ce9fcafc9593ae27
Jeffry Anderson Romero Uriol <jeffry.25.ru@gmail.com>
2025-12-03 00:40:02 -0500

    feat: Improve run_streamlit.sh to check for virtual environment and install dependencies

--- run_streamlit.sh
+++ run_streamlit.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 cd /home/jupyter/frontend
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-/opt/python-env/bin/streamlit run /home/jupyter/frontend/streamlit_app.py --server.address 0.0.0.0 --server.port 8501
+
+# Instalar dependencias si no están instaladas
+if [ ! -d "venv" ]; then
+    echo "Creando entorno virtual..."
+    python3 -m venv venv
+    source venv/bin/activate
+    echo "Instalando dependencias..."
+    pip3 install --no-cache-dir -q -r requirements.txt
+else
+    source venv/bin/activate
+fi
+
+echo "Iniciando Streamlit..."
+streamlit run /home/jupyter/frontend/streamlit_app.py --server.address 0.0.0.0 --server.port 8501
